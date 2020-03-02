44e99f291a56758fbf1dfdf7e9b857559a1c36ce
flammableassassin <flamablecreeper@gmail.com>
2020-03-02 19:23:26 +0000

    Update index.php
    Adding sql database suppport

--- public/index.php
+++ public/index.php
@@ -1,58 +1,64 @@
 <?php
 error_reporting(E_ERROR);
+$servername = "SERVER_NAME";
+$username = "USERNAME";
+$password = "PASWORD";
+$dbname = "DATABASE_NAME";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-$tokens = array(
-    "keys_here"
-);
 $sharexdir = "i/"; //File directory
 $lengthofstring = 5; //Length of file name
-//Random file name generation
+//----------------------------------------------------------------//
 function RandomString($length)
 {
     $keys = array_merge(range(0, 9) , range('a', 'z'));
 
-    for ($i = 0;$i < $length;$i++)
-    {
-        $key .= $keys[mt_rand(0, count($keys) - 1) ];
-    }
+    for ($i = 0;$i < $length;$i++)  $key .= $keys[mt_rand(0, count($keys) - 1) ];
+
     return $key;
 }
-
-//Check for token
+// Check connection
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
 if (isset($_POST['secret']))
 {
-    //Checks if token is valid
-    if (in_array($_POST['secret'], $tokens))
-    {
-        //Prepares for upload
-        $filename = RandomString($lengthofstring);
-        $target_file = $_FILES["sharex"]["name"];
-        $fileType = pathinfo($target_file, PATHINFO_EXTENSION);
+    $token = $_POST['secret'];
+    //sql req
+    $stmt = $conn->prepare("SELECT * FROM TOKENS WHERE TOKEN=?");
+    $stmt->bind_param('s', $token);
+    $stmt->execute();
+    //error here/
+    $result = $stmt->get_result(); // get the mysqli result
+    $user = $result->fetch_assoc(); // getting user
+        
+    $stmt->close();
 
-        //Accepts and moves to directory
-        if (move_uploaded_file($_FILES["sharex"]["tmp_name"], $sharexdir . $filename . '.' . $fileType))
-        {
-            //Sends info to client
-            $json->status = "OK";
-            $json->errormsg = "";
-            $json->url = $filename . '.' . $fileType;
-            //Sends json
-            echo (json_encode($json));
-        }
-        else
-        {
-            //Warning
-            echo 'File upload failed!';
-        }
+    if ($user["TOKEN"]!==$token) die("Invaild Token");
+    //user is now authenticated
+    //Prepares for upload
+    $filename = RandomString($lengthofstring);
+    $fileType = pathinfo($_FILES["sharex"]["name"], PATHINFO_EXTENSION);
+    
+
+    //Accepts and moves to directory
+    if (move_uploaded_file($_FILES["sharex"]["tmp_name"], $sharexdir . $filename . '.' . $fileType))
+    {
+        //Sends info to client
+        $json->status = "OK";
+        $json->errormsg = "";
+        $json->url = $filename . '.' . $fileType;
+        //Sends json
+        echo (json_encode($json));
     }
     else
     {
-        //Invalid key
-        echo 'Invalid Secret Key';
+        //Warning
+        echo 'File upload failed - CHMOD/Folder doesn\'t exist?';
     }
 }
-else
-{
+else{
     $file = $_GET['f'];
     $file_extension = explode(".", $file) [1];
     $array_files = scandir(getcwd() . "/i");
@@ -75,11 +81,12 @@ else
     elseif ($file === NULL)
     {
         //Warning if no uploaded data or file not found
-        echo 'This is a php file host!';
+        echo '<a href="https://github.com/Highlyflammable-Tech">Hi this is a php file host</a>';
     }
     else
     {
-        die("File not found!");
+        die("File Not Found!");
+
     }
 }
 ?>
